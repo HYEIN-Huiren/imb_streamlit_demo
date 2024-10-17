06d2863743af5a9b9b469328bb375eb6a28eafe5
HYEIN-Huiren <heyin416@gmail.com>
2024-10-17 10:52:52 +0900

    modify: add plot

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 
 # streamlit run app.py --server.port PORT_NO
 # TODO: Refactoring to React.js, FastAPI
 def main():
-    st.title('Start')
+    st.title('Start') # Header
+
+    arr = np.random.normal(1, 1, size=100)
+    fig, ax = plt.subplots()
+    ax.hist(arr, bins=20)
+
+    st.pyplot(fig) # streamlit: plot update
 
 if __name__ =="__main__":
     main()
