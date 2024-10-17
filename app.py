3bd39eb7d9000960df41039070684df081ed8d7f
HYEIN-Huiren <heyin416@gmail.com>
2024-10-17 11:02:27 +0900

    modify: add plot

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # streamlit run app.py --server.port PORT_NO
+# PORT_NO
 # TODO: Refactoring to React.js, FastAPI
 def main():
     st.title('Start') # Header
+    st.write("""Body""")
 
     arr = np.random.normal(1, 1, size=100)
     fig, ax = plt.subplots()
